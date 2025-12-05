e87c7aad30264d9c2ef4bc7ca7c8c6d589a2a504
pooprincess <872571506@qq.com>
2025-12-06 00:26:19 +0800

    修改redis地址

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -3,16 +3,23 @@ package com.hmdp.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.host}")
+    private String REDIS_IP_ADDRESS;
+
+    @Value("${spring.redis.port}")
+    private String REDIS_PORT;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.200.130:6379");
+        config.useSingleServer().setAddress("redis://" + REDIS_IP_ADDRESS + ":" + REDIS_PORT);
         return Redisson.create(config);
     }
 }
